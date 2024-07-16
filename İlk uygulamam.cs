2206e3162f433709c3d881b36da2a1222faf5c72
Mustafa Batuhan Çınar <129066476+batuhan-99@users.noreply.github.com>
2024-07-16 10:44:09 +0300

    Add files via upload

---
+++ İlk uygulamam.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mustafa Batuhan Cinar");
+            Console.WriteLine("Patika'nin uzman kadrosu ve tecrübeli eğitmenleri sayesinde yazilim dunyasina hizli bir sekilde giris " +
+                "yapabileceğim için bu bootcampteyim!");
+        }
+
+    }
+}
