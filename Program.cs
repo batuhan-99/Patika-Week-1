537f0f21ccd3c70397d9e7be0012e11f6905dc9f
Mustafa Batuhan Çınar <129066476+batuhan-99@users.noreply.github.com>
2024-07-17 17:25:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace pratikifelse
+{
+    class ifelse
+    {
+        void static Main(string[] args)
+        {
+            Console.WriteLine("Lutfen bir sayi giriniz:");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            if (sayi == 10)
+            {
+                Console.Write("Girilen sayi 10'a eşittir.");
+            }
+            else if (sayi > 10)
+            {
+                Console.Write("Girilen sayi 10'dan büyüktür.");
+            }
+            else
+            {
+                Console.Write("Girilen sayi 10'dan küçüktür.");
+            }
+
+            if (sayi % 2 == 0)
+            {
+                Console.Write("Girilen sayi çifttir.");
+            }
+            else
+            {
+                Consle.Write("Girilen sayi tektir.");
+            }
+        }
+    }
+}
