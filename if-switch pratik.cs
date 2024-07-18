fb59e777d8f644a3c512cda2f41f9c4e431ededd
Mustafa Batuhan Çınar <129066476+batuhan-99@users.noreply.github.com>
2024-07-18 17:12:18 +0300

    Add files via upload

---
+++ if-switch pratik.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Pratikifelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //Çok fazla seçenek olduğu için switch case metodunu kullandım.
+            Console.WriteLine("Ruya Manavina Hos geldiniz!");
+            Console.WriteLine("Elma = 2 TL \n" +
+                "Armut = 3 TL \n" +
+                "Çilek = 2 TL \n" +
+                "Muz = 3 TL \n" +
+                "Diger butun meyveler 4 TL");
+            Console.WriteLine("Hangi meyveyi satin almak istersiniz? (Elma, Armut, Çilek, Muz, Diğer)");
+            string meyve = Console.ReadLine();
+            meyve = meyve.ToUpper();
+            switch (meyve)
+            {
+                case "ARMUT":
+                    Console.Write("Seçtiğiniz meyvenin fiyatı 3 TL'dir.");
+                    break;
+                case "ELMA":
+                    Console.Write("Seçtiğiniz meyvenin fiyatı 2 TL'dir.");
+                    break;
+                case "ÇİLEK":
+                    Console.Write("Seçtiğiniz meyvenin fiyati 2 TL'dir.");
+                    break;
+                case "MUZ":
+                    Console.Write("Seçtiğiniz meyvenin fiyatı 3 TL'dir.");
+                    break;
+                case "DİGER":
+                    Console.Write("Diğer meyveler 4 TL");
+                    break;
+                default:
+                    Console.Write("Aradığınız meyve bulunamadı.");
+                    break;
+            }
+
+            
+        }
+    }
+}
